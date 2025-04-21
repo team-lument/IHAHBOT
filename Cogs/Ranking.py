712e90a55b8d0cb4dc135dfdc6b0b0b4bdaaa36f
Dongwan Seo <kr.ihah@kakao.com>
2025-04-21 21:27:03 +0900

    feat: RP Ranking by Server

--- Cogs/Ranking.py
+++ Cogs/Ranking.py
@@ -6,48 +6,59 @@ from module.database import getAllCharacterName, getCharacterName, searchCharact
 from module.embed import makeErrorEmbed
 from module.games import getRecordOptions
 from module.player import getRecord
-from module.ranking import getRanking_Artisan, getRanking_LP
+from module.ranking import getRanking_Artisan, getRanking_LP, getRanking_LP_Server
 from module.variables import getTierName
 
 class PreviousPage(disnake.ui.Button['RankingView']):
-	def __init__(self, userId: int, page: int, length: int, artisan: bool):
+	def __init__(self, userId: int, page: int, length: int, artisan: bool, server: int = 0):
 		self._userId = userId
 		self._page = page
 		self._length = length
 		self._artisan = artisan
+		self._server = server
 		super().__init__(emoji="◀️", style=disnake.ButtonStyle.blurple, disabled=page <= 0)
 	
 	async def callback(self, i: disnake.Interaction):
 		if self._userId == i.user.id:
 			await i.response.defer()
-			ranking = await getRanking_LP(floor((self._page-1)/2))
-			rankList = makeRanking(ranking['result'], back=True if int((self._page-1)/2) != (self._page-1)/2 else False, artisan=self._artisan)
-			await i.edit_original_message(view=RankingView(self._userId, self._page-1, self._length, rankList, self._artisan))
+			if self._server != 0:
+				ranking = (await getRanking_LP_Server(self._server))['topRanks'][self._page*25:(self._page+1)*25+1]
+				rankList = makeRanking_LP(ranking)
+			else:
+				ranking = await getRanking_LP(floor((self._page-1)/2))
+				rankList = makeRanking(ranking['result'], back=True if int((self._page-1)/2) != (self._page-1)/2 else False, artisan=self._artisan)
+			await i.edit_original_message(view=RankingView(self._userId, self._page-1, self._length, rankList, artisan=self._artisan, server=self._server))
 		else:
 			await i.response.send_message(embed=makeErrorEmbed("선택권은 메시지 주인에게만 있어요!"), ephemeral=True)
 
 class NextPage(disnake.ui.Button['RankingView']):
-	def __init__(self, userId: int, page: int, length: int, artisan: bool):
+	def __init__(self, userId: int, page: int, length: int, artisan: bool, server: int = 0):
 		self._userId = userId
 		self._page = page
 		self._length = length
 		self._artisan = artisan
+		self._server = server
 		super().__init__(emoji="▶️", style=disnake.ButtonStyle.blurple, disabled=True if page == length-1 else False)
 	
 	async def callback(self, i: disnake.Interaction):
 		if self._userId == i.user.id:
 			await i.response.defer()
-			ranking = await getRanking_LP(floor((self._page+1)/2))
-			rankList = makeRanking(ranking['result'], back=True if int((self._page+1)/2) != (self._page+1)/2 else False, artisan=self._artisan)
-			await i.edit_original_message(view=RankingView(self._userId, self._page+1, self._length, rankList, self._artisan))
+			if self._server != 0:
+				ranking = (await getRanking_LP_Server(self._server))['topRanks'][self._page*25:(self._page+1)*25+1]
+				rankList = makeRanking_LP(ranking)
+			else:
+				ranking = await getRanking_LP(floor((self._page+1)/2))
+				rankList = makeRanking(ranking['result'], back=True if int((self._page+1)/2) != (self._page+1)/2 else False, artisan=self._artisan)
+			await i.edit_original_message(view=RankingView(self._userId, self._page+1, self._length, rankList, artisan=self._artisan, server=self._server))
 		else:
 			await i.response.send_message(embed=makeErrorEmbed("선택권은 메시지 주인에게만 있어요!"), ephemeral=True)
 
 class ChangePageModal(disnake.ui.Modal):
-	def __init__(self, userId: int, page: int, length: int, artisan: bool):
+	def __init__(self, userId: int, page: int, length: int, artisan: bool, server: int = 0):
 		self._length = length
 		self._userId = userId
 		self._artisan = artisan
+		self._server = server
 		components = [
 			disnake.ui.TextInput(
 				label="페이지",
@@ -65,21 +76,26 @@ class ChangePageModal(disnake.ui.Modal):
 	async def callback(self, i: disnake.ModalInteraction):
 		self._page = int(i.text_values['page_goto'])-1
 		if self._page > self._length: await i.response.send_message(embed=makeErrorEmbed("최대 페이지를 초과할 수 없어요!"), ephemeral=True)
-		ranking = await getRanking_LP(floor((self._page)/2))
-		rankList = makeRanking(ranking['result'], back=True if int((self._page)/2) != (self._page)/2 else False, artisan=self._artisan)
-		await i.response.edit_message(view=RankingView(self._userId, self._page, self._length, rankList, self._artisan))
+		if self._server != 0:
+			ranking = (await getRanking_LP_Server(self._server))['topRanks'][self._page*25:(self._page+1)*25+1]
+			rankList = makeRanking_LP(ranking)
+		else:
+			ranking = await getRanking_LP(floor((self._page)/2))
+			rankList = makeRanking(ranking['result'], back=True if int((self._page)/2) != (self._page)/2 else False, artisan=self._artisan)
+		await i.response.edit_message(view=RankingView(self._userId, self._page, self._length, rankList, artisan=self._artisan, server=self._server))
 
 class ChangePage(disnake.ui.Button):
-	def __init__(self, userId: int, page: int, length: int, artisan: bool):
+	def __init__(self, userId: int, page: int, length: int, artisan: bool, server: int = 0):
 		self._userId = userId
 		self._page = page
 		self._length = length
 		self._artisan = artisan
+		self._server = server
 		super().__init__(label=f"{page+1} / {length-1}", style=disnake.ButtonStyle.gray)
 	
 	async def callback(self, i: disnake.Interaction):
 		if self._userId == i.user.id:
-			await i.response.send_modal(ChangePageModal(self._userId, self._page, self._length, self._artisan))
+			await i.response.send_modal(ChangePageModal(self._userId, self._page, self._length, self._artisan, self._server))
 		else:
 			await i.response.send_message(embed=makeErrorEmbed("선택권은 메시지 주인에게만 있어요!"), ephemeral=True)
 
@@ -104,11 +120,11 @@ class RankList(disnake.ui.Select):
 			await i.response.send_message(embed=makeErrorEmbed("선택권은 메시지 주인에게만 있어요!"), ephemeral=True)
 
 class RankingView(disnake.ui.View):
-	def __init__(self, userId: int, page: int, length: int, rankList: list[disnake.SelectOption], artisan: bool = False):
+	def __init__(self, userId: int, page: int, length: int, rankList: list[disnake.SelectOption], *, artisan: bool = False, server: int = 0):
 		super().__init__(timeout=None)
-		self.add_item(PreviousPage(userId, page, length, artisan))
-		self.add_item(ChangePage(userId, page, length, artisan))
-		self.add_item(NextPage(userId, page, length, artisan))
+		self.add_item(PreviousPage(userId, page, length, artisan, server))
+		self.add_item(ChangePage(userId, page, length, artisan, server))
+		self.add_item(NextPage(userId, page, length, artisan, server))
 		self.add_item(RankList(userId, rankList))
 
 class Ranking(commands.Cog):
@@ -139,18 +155,92 @@ class Ranking(commands.Cog):
 			data={
 				disnake.Locale.ko: "프리 시즌이 아닌 현재 시즌의 RP 랭킹을 불러옵니다."
 			}
-		)
+		),
+		options=[
+			disnake.Option(
+				name="server",
+				description=disnake.Localized(
+					"Server ID",
+					data={
+						disnake.Locale.ko: "서버 ID"
+					}
+				),
+				type=disnake.OptionType.integer,
+				required=False,
+				choices=[
+					disnake.OptionChoice(name=disnake.Localized(
+						"Asia",
+						data={
+							disnake.Locale.ko: "아시아"
+						}
+					), value=10),
+					disnake.OptionChoice(name=disnake.Localized(
+						"Asia2",
+						data={
+							disnake.Locale.ko: "아시아2"
+						}
+					), value=17),
+					disnake.OptionChoice(name=disnake.Localized(
+						"NorthAmerica",
+						data={
+							disnake.Locale.ko: "북미"
+						}
+					), value=12),
+					disnake.OptionChoice(name=disnake.Localized(
+						"Europe",
+						data={
+							disnake.Locale.ko: "유럽"
+						}
+					), value=13),
+					disnake.OptionChoice(name=disnake.Localized(
+						"SouthAmerica",
+						data={
+							disnake.Locale.ko: "남미"
+						}
+					), value=14)
+				]
+			)
+		]
 	)
-	async def ranking_rp_cmd(self, i: disnake.CommandInteraction):
+	async def ranking_rp_cmd(
+		self, i: disnake.CommandInteraction,
+		server: int = 10
+	):
 		await i.response.defer()
-		ranking = await getRanking_LP()
-		topMMR = ranking['topMMR']
-		rankList = makeRanking(ranking['result'])
+		serverList = {
+			10 : "아시아",
+			17 : "아시아2",
+			12 : "북미",
+			13 : "유럽",
+			14 : "남미"
+		}
+		ranking = (await getRanking_LP_Server(server))['topRanks']
+		rankList = makeRanking_LP(ranking)
+		highRank = {
+			"eternity": {
+				"mmr": ranking[300]['mmr'],
+				"rank": 300
+			},
+			"demigod": {
+				"mmr": 7800,
+				"rank": 1000
+			}
+		}
+		for x in range(1,301):
+			if ranking[x]['mmr'] < 7800:
+				highRank['eternity']['mmr'] = ranking[x-1]['mmr']
+				highRank['eternity']['rank'] = ranking[x-1]['rank']
+				break
+		for x in range(300,1000):
+			if ranking[x]['mmr'] < 7800:
+				highRank['demigod']['mmr'] = ranking[x-1]['mmr']
+				highRank['demigod']['rank'] = ranking[x-1]['rank']
+				break
 		embed = disnake.Embed(
-			title="RP 랭킹",
-			description=f"이터컷: {topMMR['top200']}점\n데미컷: {topMMR['top700']}점"
+			title=f"{serverList[server]} RP 랭킹",
+			description=f"이터니티 컷: {highRank['eternity']['mmr']}점 ({highRank['eternity']['rank']}위)\n-# 7800점 이상, 300위 이내\n\n데미갓 컷: {highRank['demigod']['mmr']}점 ({highRank['demigod']['rank']}위)\n-# 7800점 이상, 1000위 이내"
 		)
-		await i.edit_original_message(embed=embed, view=RankingView(i.user.id, 0, (ranking['page'][1]-1)*2, rankList))
+		await i.edit_original_message(embed=embed, view=RankingView(i.user.id, 0, 40, rankList, server=server))#(ranking['page'][1]-1)*2, rankList))
 	
 	async def artisan_autocomplete(i: disnake.CommandInteraction, character: str):
 		listFrom = searchCharacter(character)
@@ -207,6 +297,15 @@ def makeRanking(r: dict, back: bool = False, artisan: bool = False):
 			x = r[tmp]; rank = x['index']+1
 			if tmp > 0 and x['value'] == r[tmp-1]['value']: rank -= 1
 			rankList.append(disnake.SelectOption(label=f"#{rank} {x['playerName']}", description=f"{' '.join(getTierName(x['value'], x['queueTable']['3']['isDemigod'], x['queueTable']['3']['isEternity'])[0:1])} ({x['value']}LP)" if not artisan else f"{x['value']}점", value=x['playerId']))
+	return rankList
+
+def makeRanking_LP(r: dict):
+	rankList = []
+	for tmp in range(25):
+		if len(rankList) < 25:
+			x = r[tmp]; rank = x['rank']
+			if tmp > 0 and x['mmr'] == r[tmp-1]['mmr']: rank -= 1
+			rankList.append(disnake.SelectOption(label=f"#{rank} {x['nickname']}", description=f"{' '.join(getTierName(x['mmr'], x['mmr'] >= 7800 and x['rank'] <= 300, x['mmr'] >= 7800 and x['rank'] <= 1000)[0:1])} ({x['mmr']}LP)", value=x['userNum']))
 	return rankList
 
 def setup(bot: commands.Bot):
--- module/ranking.py
+++ module/ranking.py
@@ -1,9 +1,15 @@
 import aiohttp
-from config import AYAGG_API_URL, AYAGG_HEADER
+from config import *
+from module.variables import nowSeason
 
 async def getRanking_LP(page: int=0):
 	async with aiohttp.ClientSession(headers=AYAGG_HEADER) as session:
 		async with session.get(AYAGG_API_URL + f"/ranking/mmr/by-queue/3?page={page}") as res:
+			return await res.json()
+
+async def getRanking_LP_Server(server: int=10):
+	async with aiohttp.ClientSession(headers=API_HEADER) as session:
+		async with session.get(API_URL + f"/v1/rank/top/{await nowSeason()}/3/{server}") as res:
 			return await res.json()
 
 async def getRanking_Artisan(characterId: int, page: int=0):
