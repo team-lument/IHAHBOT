b64f1cb74c9b945625ff0d6d519fc350c9928e02
Dong Wan Seo <kr.ihah@kakao.com>
2026-03-11 03:10:28 +0900

    config

---
+++ config.inc.py
@@ -0,0 +1,26 @@
+# Discord
+TOKEN_PRODUCTION = ""
+TOKEN_TESTBUILD = ""
+
+# Koreanbots
+KOREANBOTS_TOKEN = ""
+
+# Eternal Return
+API_KEY = ""
+API_URL = "https://open-api.bser.io"
+API_HEADER = { 'x-api-key': API_KEY }
+AYAGG_API_KEY = "Bearer "
+AYAGG_API_URL = "https://api.aya.gg"
+AYAGG_HEADER = { 'Authorization': AYAGG_API_KEY }
+
+# Trello
+TRELLO_KEY = ""
+TRELLO_TOKEN = ""
+
+# Twitch
+TWITCH_APP_ID = ""
+TWITCH_APP_SECRET = ""
+
+# Webhook
+ERROR_WEBHOOK_URL = ""
+LOG_WEBHOOK_URL = ""
